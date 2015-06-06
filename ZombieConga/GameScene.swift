017998ed81af90119040191836493e4a4bc43447
Chris Archibald <carchi8py@gmail.com>
2015-06-06 14:34:31 -0700

    Fixed image bug, added zombie movement.

--- ZombieConga/GameScene.swift
+++ ZombieConga/GameScene.swift
@@ -9,22 +9,26 @@
 import SpriteKit
 
 class GameScene: SKScene {
+    
+    let zombie = SKSpriteNode(imageNamed: "zombie1")
+    
     override func didMoveToView(view: SKView) {
         backgroundColor = SKColor.whiteColor()
         
         //Set the background for the game
-        let background = SKSpriteNode(imageNamed: "images/background1")
+        let background = SKSpriteNode(imageNamed: "background1")
         background.position = CGPoint(x: size.width/2, y: size.height/2)
         background.zPosition = -1
         addChild(background)
         
         //Adding a zombie to the game
-        let zombie = SKSpriteNode(imageNamed: "zombie1")
         zombie.position = CGPoint(x: 400, y: 400)
         zombie.zPosition = 1
-        //Make the zombie 2 timex larger
-        zombie.xScale = 2.0
-        zombie.yScale = 2.0
         addChild(zombie)
+    }
+    
+    override func update(currentTime: NSTimeInterval) {
+        zombie.position = CGPoint(x: zombie.position.x + 4,
+                                  y: zombie.position.y)
     }
 }
